e0c98260a456aac5dbdb8028756f6d81ff4c21f7
Abdulbasit <you@example.com>
2025-11-07 15:43:35 +0100

    new file

---
+++ lib/widgets/basit.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Basit extends StatefulWidget {
+  const Basit({super.key});
+
+  @override
+  State<Basit> createState() => _BasitState();
+}
+
+class _BasitState extends State<Basit> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
